76c20822510975c4aa013b57a37dbb6aeb94eb1f
Artem Starosvetskiy <artyom.stv@gmail.com>
2016-12-13 15:10:51 +0300

    Union implementations of `Equatable` and `Hashable` protocols in `BankCardNumber` into one file.
    The reason to union implementations in one file is that `Hashable` protocol inherits `Equatable` protocol.

--- TextInputKit/TextInputKit/Code/SpecializedTextInput/CardNumber/BankCardNumber+Hashable.swift
+++ TextInputKit/TextInputKit/Code/SpecializedTextInput/CardNumber/BankCardNumber+Hashable.swift
@@ -6,10 +6,18 @@
 //  Copyright © 2016 Artem Starosvetskiy. All rights reserved.
 //
 
+extension BankCardNumber : Equatable {
+
+    public static func ==(lhs: BankCardNumber, rhs: BankCardNumber) -> Bool {
+        return lhs.digitsString == rhs.digitsString
+    }
+
+}
+
 extension BankCardNumber : Hashable {
 
     public var hashValue: Int {
         return digitsString.hashValue
     }
-
+    
 }
