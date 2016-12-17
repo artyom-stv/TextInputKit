060948ec6167f1f762e4fa235bde99ba0c16768e
Artem Starosvetskiy <artyom.stv@gmail.com>
2016-12-17 18:22:19 +0300

    Document TextInputFormat+Bind extension.

--- TextInputKit/TextInputKit/Code/TextInputFormat/TextInputFormat+Bind.swift
+++ TextInputKit/TextInputKit/Code/TextInputFormat/TextInputFormat+Bind.swift
@@ -8,16 +8,31 @@
 
 import Foundation
 
+/// A class representing a text input component (e.g. UITextField) should implement the `TextInputFormatBindable` protocol
+/// to be able to be bound to a text input format using the `TextInputFormat.bind(to:)` API.
 public protocol TextInputFormatBindable {
 
+    /// Binds the text input format to `self`.
+    ///
+    /// - Parameters:
+    ///   - format: The text input format.
+    /// - Returns: The created `TextInputBinding`.
     func bind<Value>(format: TextInputFormat<Value>) -> TextInputBinding<Value>
 
 }
 
 public extension TextInputFormat {
 
-    func bind<TextInput: TextInputFormatBindable>(to textInput: TextInput) -> TextInputBinding<Value> {
-        return textInput.bind(format: self)
+    /// Binds the text input format to a text field, which conforms to the `TextInputFormatBindable` protocol.
+    ///
+    /// - Note:
+    ///   This method only delegates the work to the `TextInputFormatBindable` protocol implementation.
+    ///
+    /// - Parameters:
+    ///   - textField: The text field conforming to the `TextInputFormatBindable` protocol.
+    /// - Returns: The created `TextInputBinding`.
+    func bind<TextField: TextInputFormatBindable>(to textField: TextField) -> TextInputBinding<Value> {
+        return textField.bind(format: self)
     }
 
 }
