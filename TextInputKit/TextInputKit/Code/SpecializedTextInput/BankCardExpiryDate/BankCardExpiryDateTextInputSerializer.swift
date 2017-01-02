f527e07a53e3813c5231c0fe8880992d8505c1df
Artem Starosvetskiy <artyom.stv@gmail.com>
2017-01-02 18:45:56 +0300

    Fix a bug in `BankCardExpiryDateTextInputSerializer`.

--- TextInputKit/TextInputKit/Code/SpecializedTextInput/BankCardExpiryDate/BankCardExpiryDateTextInputSerializer.swift
+++ TextInputKit/TextInputKit/Code/SpecializedTextInput/BankCardExpiryDate/BankCardExpiryDateTextInputSerializer.swift
@@ -20,26 +20,64 @@ final class BankCardExpiryDateTextInputSerializer : TextInputSerializer<BankCard
     }
 
     override func value(for string: String) throws -> BankCardExpiryDate {
-        let components = string.components(separatedBy: StringUtils.slashCharacters)
-
-        guard components.count == 2 else {
-            fatalError()
-        }
-
-        let monthComponent = components[0]
-        let yearComponent = components[1]
-
-        guard (1...2).contains(monthComponent.unicodeScalars.count), yearComponent.unicodeScalars.count == 2 else {
+        guard let stringComponents = self.stringComponents(from: string) else {
             throw BankCardExpiryDateTextInputError.incompleteTextInput
         }
 
-        guard let month = Int(monthComponent), let lastTwoDigitsOfYear = Int(yearComponent) else {
-            fatalError()
-        }
+        let intComponents = self.intComponents(from: stringComponents)
 
-        return try BankCardExpiryDate(month: month, lastTwoDigitsOfYear: lastTwoDigitsOfYear)
+        return try BankCardExpiryDate(
+            month: intComponents.month,
+            lastTwoDigitsOfYear: intComponents.lastTwoDigitsOfYear)
     }
 
     private let options: BankCardExpiryDateTextInputOptions
+
+}
+
+private extension BankCardExpiryDateTextInputSerializer {
+
+    typealias ParsedStringComponents = (month: String, lastTwoDigitsOfYear: String)
+
+    typealias ParsedIntComponents = (month: Int, lastTwoDigitsOfYear: Int)
+
+    func stringComponents(from string: String) -> ParsedStringComponents? {
+        let components = string.components(separatedBy: StringUtils.slashCharacters)
+
+        guard components.count <= 2 else {
+            fatalError("`BankCardExpiryDateTextInputFormatter` shouldn't have formed a string with more than one 'slash' character.")
+        }
+
+        guard components.count == 2 else {
+            return nil
+        }
+
+        // Currently, `BankCardExpiryDateTextInputSerializer` supports only bank card expiry dates with two digits
+        // in a year component, e.g. "01/17".
+        // In future, we might support four-digit year components, e.g. "01/2017".
+        let parsedComponents = (month: components[0], lastTwoDigitsOfYear: components[1])
+
+        // A month component can be one character long to accept text input like "1/17".
+        guard (1...2).contains(parsedComponents.month.unicodeScalars.count) else {
+            return nil
+        }
+
+        guard parsedComponents.lastTwoDigitsOfYear.unicodeScalars.count == 2 else {
+            return nil
+        }
+
+        return parsedComponents
+    }
+
+    func intComponents(from stringComponents: ParsedStringComponents) -> ParsedIntComponents {
+        guard
+            let monthComponent = Int(stringComponents.month),
+            let yearComponent = Int(stringComponents.lastTwoDigitsOfYear)
+            else {
+                fatalError("`BankCardExpiryDateTextInputFormatter` should have formed a string with only digit characters in date components.")
+        }
+
+        return (month: monthComponent, lastTwoDigitsOfYear: yearComponent)
+    }
 
 }
