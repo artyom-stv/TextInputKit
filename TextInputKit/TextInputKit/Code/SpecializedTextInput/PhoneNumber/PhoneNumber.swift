197f4164bac839a117a424afd902f1af2d22e30c
Artem Starosvetskiy <artyom.stv@gmail.com>
2017-01-13 22:31:37 +0300

    Add a public initializer to `PhoneNumber`.

--- TextInputKit/TextInputKit/Code/SpecializedTextInput/PhoneNumber/PhoneNumber.swift
+++ TextInputKit/TextInputKit/Code/SpecializedTextInput/PhoneNumber/PhoneNumber.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Artem Starosvetskiy. All rights reserved.
 //
 
-import Foundation
+import PhoneNumberKit
 
 public struct PhoneNumber {
 
@@ -15,6 +15,41 @@ public struct PhoneNumber {
     }
 
     let pnkPhoneNumber: PNKPhoneNumber
+
+}
+
+extension PhoneNumber {
+
+    /// Creates a `PhoneNumber` by a phone number string.
+    ///
+    /// - Note: Only phone numbers starting with "+" are supported.
+    ///
+    /// - Parameters:
+    ///   - phoneNumberString: A string representing a phone number.
+    /// - Throws:
+    ///   - `TextInputKitError.missingFramework` if PhoneNumberKit framework isn't loaded.
+    ///   - `PhoneNumberTextInputError.invalidInput` if the provided string doesn't represent a valid phone number.
+    public init(_ phoneNumberString: String) throws {
+        try PhoneNumberKit.checkThatFrameworkIsLoaded()
+
+        if let firstUnicodeScalar = phoneNumberString.unicodeScalars.first, !StringUtils.isPlus(firstUnicodeScalar) {
+            throw PhoneNumberTextInputError.invalidInput(.nationalNumbersAreNotSupported)
+        }
+
+        let pnkPhoneNumber: PNKPhoneNumber
+        do {
+            let phoneNumberKit = PhoneNumberKit.cached.instance
+            pnkPhoneNumber = try phoneNumberKit.parse(phoneNumberString)
+        } catch let error as PhoneNumberError {
+            throw PhoneNumberTextInputError.from(error)
+        }
+
+        if pnkPhoneNumber.numberExtension != nil {
+            throw PhoneNumberTextInputError.invalidInput(.numberExtensionAreNotSupported)
+        }
+
+        self.init(pnkPhoneNumber)
+    }
 
 }
 
