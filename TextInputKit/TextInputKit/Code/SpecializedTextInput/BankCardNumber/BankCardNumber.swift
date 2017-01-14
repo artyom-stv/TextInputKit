7195117a3ea5c86c9738222be8f1de31e2d727e1
Artem Starosvetskiy <artyom.stv@gmail.com>
2017-01-14 21:10:10 +0300

    Union the `BankCardNumber` declaration and the implementation of `Hashable` protocol in `BankCardNumber` in one file.

--- TextInputKit/TextInputKit/Code/SpecializedTextInput/BankCardNumber/BankCardNumber.swift
+++ TextInputKit/TextInputKit/Code/SpecializedTextInput/BankCardNumber/BankCardNumber.swift
@@ -99,3 +99,19 @@ extension BankCardNumber {
     }
     
 }
+
+extension BankCardNumber : Equatable {
+
+    public static func ==(lhs: BankCardNumber, rhs: BankCardNumber) -> Bool {
+        return lhs.digitsString == rhs.digitsString
+    }
+
+}
+
+extension BankCardNumber : Hashable {
+
+    public var hashValue: Int {
+        return digitsString.hashValue
+    }
+    
+}
