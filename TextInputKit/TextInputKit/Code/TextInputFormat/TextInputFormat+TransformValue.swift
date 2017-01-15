5d6b1dd6bf8b31a8199ea67b5eab57dd28964994
Artem Starosvetskiy <artyom.stv@gmail.com>
2017-01-15 16:26:36 +0300

    Add a convenient method for transforming a value of a `TextInputFormat`.

--- TextInputKit/TextInputKit/Code/TextInputFormat/TextInputFormat+TransformValue.swift
+++ TextInputKit/TextInputKit/Code/TextInputFormat/TextInputFormat+TransformValue.swift
@@ -13,9 +13,45 @@ public extension TextInputFormat {
     public typealias DirectValueTransform<Result> = (Value) throws -> Result
     public typealias ReverseValueTransform<Result> = (Result) -> Value
 
+    /// Creates a `TextInputFormat` which transforms the values produced by the source serializer
+    /// (the serializer of the callee format).
+    ///
+    /// - Parameters:
+    ///   - directTransform: The transformation from a source value to a resulting value.
+    ///   - reverseTransform: The transformation from a resulting value to a source value.
+    /// - Returns: The created `TextInputFormatter`.
     func transformValue<Result>(
         direct directTransform: @escaping DirectValueTransform<Result>,
         reverse reverseTransform: @escaping ReverseValueTransform<Result>) -> TextInputFormat<Result> {
+
+        return TextInputFormat.from(
+            self.serializer.map(direct: directTransform, reverse: reverseTransform),
+            self.formatter)
+    }
+
+}
+
+public extension TextInputFormat {
+
+    public typealias NonThrowingDirectValueTransform<Result> = (Value) -> Result?
+
+    /// Creates a `TextInputFormat` which transforms the values produced by the source serializer
+    /// (the serializer of the callee format).
+    ///
+    /// - Parameters:
+    ///   - directTransform: The transformation from a source value to a resulting value.
+    ///   - reverseTransform: The transformation from a resulting value to a source value.
+    /// - Returns: The created `TextInputFormatter`.
+    func transformValue<Result>(
+        direct nonThrowingDirectTransform: @escaping NonThrowingDirectValueTransform<Result>,
+        reverse reverseTransform: @escaping ReverseValueTransform<Result>) -> TextInputFormat<Result> {
+
+        let directTransform: DirectValueTransform<Result> = { value in
+            guard let result = nonThrowingDirectTransform(value) else {
+                throw TextInputKitError.unknown
+            }
+            return result
+        }
 
         return TextInputFormat.from(
             self.serializer.map(direct: directTransform, reverse: reverseTransform),
